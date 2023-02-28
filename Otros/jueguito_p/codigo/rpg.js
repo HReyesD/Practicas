17c2dee2d6cc2d5b913b8298f32a1cb68b862e7b
Henry Reyes <H.reyes.redes@hotmail.com>
2023-02-27 23:20:25 -0400

    Commit 2

--- Otros/jueguito_p/codigo/rpg.js
+++ Otros/jueguito_p/codigo/rpg.js
@@ -1,1 +1,44 @@
-alert("Hola, Manco.")
+//alert("Hola, Manco.")
+
+window.addEventListener("load",Iniciar)
+   
+    function Iniciar () {
+
+        let bClase = document.getElementById('b_clase')
+        
+            bClase.addEventListener('click', sClase)
+
+    }
+
+    function sClase(){
+
+        let picaro = document.getElementById('picaro')
+        let guerrero = document.getElementById('guerrero')
+        let mago = document.getElementById('mago')
+        let tu_vida = document.getElementById("tu_vida")
+        let vida_enemiga = document.getElementById("vida_enemiga")
+        let tu_clase = document.getElementById('tu_clase')
+
+        if (picaro.checked) 
+            {alert("Elegiste Picaro")} 
+        else if (mago.checked) 
+            {alert("Elegiste Mago")}
+        else if (guerrero.checked) 
+            {alert("Elegiste Guerrero")}
+        else(alert("Elige Una Clase"))
+
+        if (picaro.checked) 
+        {tu_clase.innerHTML = 'Picaro'}
+        else if (mago.checked)
+        {tu_clase.innerHTML = 'Mago'}
+        else if (guerrero.checked)
+        (tu_clase.innerHTML = 'Guerrero')
+        
+    }
+
+
+
+
+
+
+
